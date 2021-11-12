1d12a4d542c52d4cdfbf19be40583f842255860d
DESKTOP-LO51G0C\sergi <verasergio700@gmail.com>
2021-11-11 19:20:28 -0500

    se llama a la api de sololearm y se guardan los datos

--- backend/src/controllers/Candidate.js
+++ backend/src/controllers/Candidate.js
@@ -1,10 +1,13 @@
 const Profile = require('../db/models/Profile');
 const User = require('../db/models/User');
+const Result = require('../db/models/Result');
 const multer = require('multer');
 const mimeTypes = require('mime-types')
 const candidateRouter = require('express').Router()
+const request = require('request');
+const { ConnectionStates } = require('mongoose');
 
-// Upload file PDF
+// UPLOAD FILE PDF
 const storage = multer.diskStorage({
         destination: 'uploads/',
         filename: function (req, file,cb) {
@@ -16,9 +19,95 @@ const upload = multer({
         storage: storage
 })
 
+// SAVE AND UPDATE SOLOLEARN DATA
+candidateRouter.get('/sololearm/:id', async (req, res) => {
 
-candidateRouter.get('/', async (req, res, next) => {
-        res.send("the router is fine")
+        var id = req.params.id
+        const perfiles = await Profile.find({"user_id":id})
+        const params = JSON.stringify(perfiles)
+        const json = JSON.parse(params)
+        for (x of json) {
+               var usersololearm = (x.soloLearnProfile);
+        }
+        if(usersololearm === undefined){
+                console.log("este usuario no tiene perfil de sololearm")
+        }else{
+                const calificationUpdate = await Result.find({"user_id":id})
+                const params2 = JSON.stringify(calificationUpdate)
+                const json2 = JSON.parse(params2)
+                for (y of json2) {
+                        var users_id = (y.user_id).toString();
+                }
+                request(`https://api.sololearn.repl.co/profile/${usersololearm}`, async (err,response,body)=>{
+                        if (!err){
+                                const user = JSON.parse(body);
+                                var name = user.userDetails.name
+                                var html = 0;
+                                var css = 0;
+                                var javascript = 0;
+                                var python = 0;
+                                for (let i = 0; i < user.coursesProgress.length; i++) {
+                                        if(user.coursesProgress[i].courseId === 1014){
+                                                html = user.coursesProgress[i].progress;
+                                        }else if(user.coursesProgress[i].courseId === 1024){
+                                                javascript = user.coursesProgress[i].progress;
+                                        }else if(user.coursesProgress[i].courseId === 1023){
+                                                css = user.coursesProgress[i].progress;
+                                        }else if(user.coursesProgress[i].courseId === 1073){
+                                                python = user.coursesProgress[i].progress;
+                                        }                           
+                                }
+                                var p = (html+css+javascript+python)/4;
+
+                                const{  user_id = id,  
+                                        userFullName = name, 
+                                        htmlScore = html,
+                                        cssScore = css,
+                                        javascriptScore  = javascript,
+                                        pythonScore = python,
+                                        soloLearnScore = p.toFixed(2)
+                                }= req.body
+                                           
+                                const usersolo = new Result({
+                                        user_id,  
+                                        userFullName, 
+                                        htmlScore,
+                                        cssScore,
+                                        javascriptScore,
+                                        pythonScore,
+                                        soloLearnScore
+                                });
+                                if(users_id === undefined) {
+                                        await usersolo.save();
+                                }else{
+                                        await Result.updateOne({"user_id":id}, {
+                                                $set: {
+                                                    htmlScore: html,
+                                                    cssScore: css,
+                                                    javascriptScore: javascript,
+                                                    pythonScore: python,
+                                                    soloLearnScore: p.toFixed(2)
+                                                }
+                                        })
+
+                                        console.log("Updated successfully")
+                                }       
+                          
+                        }else{
+                                console.log("Error" + err.message);
+                        }
+                })
+                
+        }
+            res.send("seving datas")
+})
+
+// GET CALIFACATION
+candidateRouter.get('/calification', async (req, res) => {
+
+       const calification = await Result.find()
+       res.send(calification)
+         
 })
 
 // CREATES A NEW USER
@@ -95,7 +184,12 @@ candidateRouter.post('/profile',upload.single('pdf'), async (req, res, next) => 
 
 candidateRouter.get('/candidate', async (req, res) => {
         const candidates = await User.find()
+        
         res.send(candidates);
+       
 })
+
+
+
 
 module.exports = candidateRouter
