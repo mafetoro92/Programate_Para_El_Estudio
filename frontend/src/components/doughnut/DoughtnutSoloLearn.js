3879d2de5078b96cc102787b7b9404b88e378835
Zayda Rivera <zayda07@misena.edu.co>
2021-11-16 11:59:32 -0500

    se realiza cambios a perfil aspirante archivo doughnut

--- frontend/src/components/doughnut/DoughtnutSoloLearn.js
+++ frontend/src/components/doughnut/DoughtnutSoloLearn.js
@@ -1,7 +1,43 @@
 import React from "react";
-import { Doughnut } from "react-chartjs-2";
+import { Chart } from "chart.js";
+import ChartDataLabels from "chartjs-plugin-datalabels";
+import { Doughnut, defaults } from "react-chartjs-2";
 import "./DoughtnutSoloLearn.scss";
 import "chart.piecelabel.js";
+
+Chart.register(ChartDataLabels);
+Chart.defaults.plugins.datalabels;
+defaults.plugins.legend.position = "bottom";
+
+const pruebas1 = [
+    {
+        user_id: 1,
+        userFullName: "Pedro",
+        htmlScore: 23,
+        cssScore: 25,
+        javascriptScore: 27,
+        pythonScore: 34,
+        Score: 28,
+        soloLearnScore: 28,
+        personalProfileScore: 28,
+        motivationScore: 28,
+        finalScore: 23,
+    },
+    {
+        user_id: 2,
+        userFullName: "Pedro",
+        htmlScore: 23,
+        cssScore: 25,
+        javascriptScore: 27,
+        pythonScore: 34,
+        Score: 28,
+        soloLearnScore: 28,
+        personalProfileScore: 28,
+        motivationScore: 28,
+        finalScore: 23,
+    },
+];
+
 const data = {
     datasets: [
         {
@@ -12,26 +48,27 @@ const data = {
         },
     ],
     labels: ["Progreso", "Pendiente"],
-    subtitles: [
-        {
-            text: "71% Positive",
-            verticalAlign: "center",
-            fontSize: 24,
-            dockInsidePlotArea: true,
-        },
-    ],
 };
 
+const options = {
+    maintainAspectRatio: true,
+    responsive: false,
+    plugins: {
+        datalabels: {
+            color: "#ffffff",
+            formatter: function (value, context) {
+                return Math.round(value) + "%";
+            },
+        },
+    },
+};
 const DoughtnutSoloLearn = () => {
     return (
         <div className="section__dashboard">
             <div className="content">
                 <h2>SoloLearn</h2>
-                <Doughnut data={data} />
-                <div className="legend">
-                    <i className="fas fa-circle html"></i>
-                    Html
-                </div>
+                <Doughnut data={data} options={options} />
+                <div className="legend">html</div>
             </div>
         </div>
     );
