215228eeda8daa3d3cfc5ab5f6c7100e91725e99
Yeferson Rojas Olcunche <yefersonolcunche@gmail.com>
2021-11-16 14:50:21 -0500

    Merging backend daniel & yeferson

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,18 +1,6 @@
 const { Router } = require('express')
 
 const candidateRouter = require('../controllers/candidate')
-<<<<<<< HEAD
-const admin_y = require('../controllers/admin')
-const admin_dRouter = require('../controllers/admin_d')
-const router = Router()
-
-router.use('/api/candidate', candidateRouter);
-router.use('/api/admin', admin_y, admin_dRouter);
-
-
-module.exports = router
-
-=======
 const adminRouter = require('../controllers/admin')
 const staffRouter = require('../controllers/staff')
 const router = Router()
@@ -20,6 +8,5 @@ const router = Router()
 router.use('/api/candidate', candidateRouter)
 router.use('/api/admin', adminRouter)
 router.use('/api/staff', staffRouter)
->>>>>>> 9dd96099426b41082e69c0c6c882e2493890fb94
 
 module.exports = router
