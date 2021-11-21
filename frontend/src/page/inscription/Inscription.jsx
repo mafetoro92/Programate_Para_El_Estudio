58140794613aeafddb8ad0d92d44076e08ebb18a
Mafe Toro <mtoro0926@gmail.com>
2021-11-20 20:08:43 -0500

    Parametrization

---
+++ frontend/src/page/inscription/Inscription.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import "./Inscription.scss"
+import Parameterization from "../../components/parameterization/Parameterization"
+
+
+const Inscription = () => {
+    return (
+        <div>
+            <div className="section__aspirants">
+                <div className="section__content d-flex justify-content-between">
+                    <span className="upperCase bold">Parametrización</span>
+                    <div className="box__content">
+                        <span className='text-crumbs bold-500'>Programate</span>
+                        <i class="fas fa-chevron-right subtitle" />
+                        <span className='text-crumbs'>Parametrización</span>
+                    </div>
+                </div>
+                <Parameterization/>
+            </div>
+        </div>
+    )
+}
+
+export default Inscription
