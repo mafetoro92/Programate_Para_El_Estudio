519b53e21003ebcd8a43f4b66270e7dbef9c2a92
Hiromi Acosta <mafequevedo1@gmail.com>
2021-11-27 09:24:36 -0500

    merge final

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,15 +1,15 @@
-const { Router } = require('express')
+const { Router } = require("express");
 
-const candidateRouter = require('../controllers/candidate')
-const adminRouter = require('../controllers/admin')
-const staffRouter = require('../controllers/staff')
-const userRouter = require('../controllers/user')
-    
-const router = Router()
+const candidateRouter = require("../controllers/candidate");
+const adminRouter = require("../controllers/admin");
+const staffRouter = require("../controllers/staff");
+const userRouter = require("../controllers/user");
 
-router.use('/api/candidate', candidateRouter)
-router.use('/api/admin', adminRouter)
-router.use('/api/staff', staffRouter)
-router.use('/api/user', userRouter)
+const router = Router();
 
-module.exports = router
+router.use("/api/candidate", candidateRouter);
+router.use("/api/admin", adminRouter);
+router.use("/api/staff", staffRouter);
+router.use("/api/user", userRouter);
+
+module.exports = router;
