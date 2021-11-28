1e708db4dd5d0285d28184bce6e83536e1b16fe3
Hiromi Acosta <mafequevedo1@gmail.com>
2021-11-28 09:17:33 -0500

    merge

--- frontend/src/page/citation/InterviewDay.js
+++ frontend/src/page/citation/InterviewDay.js
@@ -5,30 +5,26 @@ import CalendarView from "../../components/calendar/CalendarView";
 import DeleteEventFab from "../../components/calendar/DeleteEventFab";
 
 const InterviewDay = () => {
-    
-
-    const { activeEvent } = useSelector((state) => state.calendar)
-  return (
-    <div className="interview">
-      <div className="section__content d-flex justify-content-between">
-        <span className="upperCase bold">
-          Agenda de Entrevista y assessment
-        </span>
-        <div className="box__content">
-          <span className="text-crumbs bold-500">Programate</span>
-          <i className="fas fa-chevron-right subtitle" />
-          <span className="text-crumbs">Entrevista</span>
+    const { activeEvent } = useSelector((state) => state.calendar);
+    return (
+        <div className="interview">
+            <div className="section__content d-flex justify-content-between">
+                <span className="upperCase bold">
+                    Agenda de Entrevista y assessment
+                </span>
+                <div className="box__content">
+                    <span className="text-crumbs bold-500">Programate</span>
+                    <i className="fas fa-chevron-right subtitle" />
+                    <span className="text-crumbs">Entrevista</span>
+                </div>
+            </div>
+            <div className="calendar mt-4">
+                <CalendarView />
+                <AddNewFab />
+                {activeEvent && <DeleteEventFab />}
+            </div>
         </div>
-      </div>
-      <div className="calendar mt-4">
-
-            <CalendarView />
-            <AddNewFab/>
-            {activeEvent && <DeleteEventFab />}
-
-      </div>
-    </div>
-  );
+    );
 };
 
 export default InterviewDay;
